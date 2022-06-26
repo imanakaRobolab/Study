da9ae89d29678491bfb7611654c4688a1dc9c726
imanakaRObolab <imanaka-m@roboken.iit.tsukuba.ac.jp>
2022-06-26 12:39:35 +0900

    complete 目標1

--- javascript/chartjs/javascript/chart_js3_data_draw.js
+++ javascript/chartjs/javascript/chart_js3_data_draw.js
@@ -1,31 +1,35 @@
-<script>
 const ctx = document.getElementById('myChart').getContext('2d');
+const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+let insert_data_count = 0;
+const datasets = [12, 19, 3, 5, 2, 3];
+const config = {
+    labels: labels,
+    datasets: [{
+        label: '# of Votes',
+        data: datasets,
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+}
+
 const myChart = new Chart(ctx, {
     type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+    data: config,
     options: {
         scales: {
             y: {
@@ -34,4 +38,32 @@ const myChart = new Chart(ctx, {
         }
     }
 });
-</script>
+
+function helloworld(){
+    const input_elem = document.getElementById('inputID');
+    const graphLabel = document.getElementById('textName');
+    if(input_elem.value =='' && graphLabel.value == ''){
+        alert('please input Value field and Graph Label field');
+        return;
+     }
+    else if(input_elem.value ==''){
+       alert('please input Value field');
+       return;
+    }
+    else if(graphLabel.value == ''){
+        alert('please Graph Label field');
+       return;
+    }
+    datasets.push(parseInt(input_elem.value));
+    myChart.data.datasets[0].data = datasets;
+    if(graphLabel.value == undefined||graphLabel.value ==''){
+    labels.push('Test' + insert_data_count);
+    myChart.data.labels = labels;
+    insert_data_count += 1;
+    }
+    else{
+        labels.push(graphLabel.value);
+        myChart.data.labels = labels;
+    }
+    myChart.update();
+}
